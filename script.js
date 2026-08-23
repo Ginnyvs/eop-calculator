54b4210e34f00a4af312ba63203104e9bdca8616
Ginnyvs <ginny16.v@gmail.com>
2026-08-23 19:42:48 -0300

    Excluir script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const materialWoodcutting = [
-  { name: "Birch Tree", levelRequired: 1, xp: 10 },
-  { name: "Maple Tree", levelRequired: 10, xp: 35 },
-  { name: "Oak Tree", levelRequired: 20, xp: 120 },
-  { name: "Cypress Tree", levelRequired: 30, xp: 325 }, 
-  { name: "Pine Tree", levelRequired: 40, xp: 575 },
-  { name: "Fir Tree", levelRequired: 50, xp 875 }
-];
